6c41705e8d2636a481a9958f023529eba3c95dad
radinan <radina.nuneva@gmail.com>
2022-06-22 21:56:18 +0300

    disable cors interception

--- merchandise/src/main/java/com/fmi/merchandise/MerchandiseApplication.java
+++ merchandise/src/main/java/com/fmi/merchandise/MerchandiseApplication.java
@@ -3,15 +3,27 @@ package com.fmi.merchandise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.fmi.merchandise"})
 @EnableJpaRepositories(basePackages = {"com.fmi.merchandise"})
 public class MerchandiseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MerchandiseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MerchandiseApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
